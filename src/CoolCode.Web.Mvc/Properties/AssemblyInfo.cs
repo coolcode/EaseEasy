98e90d76cc711926d419e195af807edbeaa25953
Bruce Lee <coolcode@live.com>
2012-03-04 00:24:13 +0800

    修正版本号

--- src/CoolCode.Web.Mvc/Properties/AssemblyInfo.cs
+++ src/CoolCode.Web.Mvc/Properties/AssemblyInfo.cs
@@ -1,35 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CoolCode.Web.Mvc")]
-[assembly: AssemblyDescription("Ease&amp;Easy ASP.NET MVC Framework")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bruce Lee")]
-[assembly: AssemblyProduct("CoolCode.Web.Mvc")]
-[assembly: AssemblyCopyright("Copyright © CoolCode 2009 - 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e1cb847d-f4ab-48c0-84e5-c97aded6f1e2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+using System.Runtime.InteropServices; 
+[assembly: AssemblyTitle("CoolCode.Web.Mvc")] 
+[assembly: AssemblyProduct("CoolCode.Web.Mvc")] 
+[assembly: Guid("e1cb847d-f4ab-48c0-84e5-c97aded6f1e2")] 
